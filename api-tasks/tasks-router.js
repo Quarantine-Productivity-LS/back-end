7e96b75074dc7f731864e22e3f2bef206142854e
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-03-30 17:37:42 -0400

    added a router for tasks and the user can now view tasks, add, edit, and delete tasks

---
+++ api-tasks/tasks-router.js
@@ -0,0 +1,77 @@
+const router = require('express').Router()
+
+const Tasks = require('./tasks-model.js')
+
+//get all tasks
+router.get('/', (req, res) => {
+  Tasks.findTasks()
+    .then(tasks => {
+      res.status(200).json(tasks)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'cannot get tasks' })
+    })
+})
+
+//get task by id
+router.get('/:id', (req, res) => {
+  Tasks.findTaskById()
+    .then(task => {
+      res.status(200).json(task)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'cannot get task' })
+    })
+})
+
+//delete task
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  Tasks.removeTask(id)
+    .then(count => {
+      res.status(204).json(count)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'cannot remove task' })
+    })
+})
+
+//add task
+router.post('/', (req, res) => {
+  Tasks.addTask(req.body)
+    .then(task => {
+      res.status(201).json(task)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'cannot add task' })
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const changes = req.body
+
+  Tasks.findTaskById(id)
+    .then(task => {
+      if (task) {
+        Tasks.editTask(changes, id).then(() => {
+          Tasks.findTaskById(id).then(task => {
+            res.status(201).json(task)
+          })
+        })
+      } else {
+        res.status(404).json({ error: "couldn't find task with that id" })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "server error couldn't find task" })
+    })
+})
+
+module.exports = router;
